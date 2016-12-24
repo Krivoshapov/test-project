204dd4a64b7957bd6c56fe4c6f3086331603de89
Krivoshapov <mikhail.krivoshapov@gmail.com>
2016-12-24 16:47:02 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
+
 using namespace std;
 
 struct whore{
-    char name;
-    char preferences;
+    string name;
+    string preferences;
     int price;
     float boobiez_size;
     float vaginas_depth;
@@ -15,47 +16,47 @@ int main(int argc, char** argv) {
     isulu.name = "Isulu";
     isulu.preferences = "blowjob, anal, rimjob";
     isulu.price = 3000;
-    isulu.boobiez_size = 1,00;
-    isulu.vaginas_depth = 20,00;
+    isulu.boobiez_size = 1.00;
+    isulu.vaginas_depth = 20.00;
     
     whore nastya;
     nastya.name = "Nastya";
     nastya.preferences = "hardcore, doggie-style, petting";
     nastya.price = 10000;
-    nastya.boobiez_size = 4,00;
-    nastya.vaginas_depth = 30,00;
+    nastya.boobiez_size = 4.00;
+    nastya.vaginas_depth = 30.00;
     
     whore shramkosmum;
     shramkosmum.name = "Shramko's mum";
     shramkosmum.preferences = "the missionaire pose";
     shramkosmum.price = 666;
-    shramkosmum.boobiez_size = 1,488;
-    shramkosmum.vaginas_depth = 8,00;
+    shramkosmum.boobiez_size = 1.488;
+    shramkosmum.vaginas_depth = 7.5;
     
-    char pseudonym;
-    cout << "Choose the name of whore ur interested in: Isulu, Nastya or Shramko's mum: ";
+    string pseudonym;
+    cout << "Choose the name of whore ur interested in: Isulu, Nastya or Shramko's mum: \n ";
     cin >> pseudonym;
 
-    if (pseudonym = "Isulu"){
-        cout << "Whore's name:" << isulu.name << endl;
-        cout << "Favourite sort of sex:" << isulu.preferences << endl;
-        cout << "One hour costs:" << isulu.price << "roubles" << endl;
-        cout << "The size of tits:" << isulu.boobiez_size << endl;
-        cout << "Her pussy can receive a cock with length equal to" << isulu.vaginas_depth << "cm" << endl;
+    if (pseudonym == "Isulu"){
+        cout << "Whore's name: " << isulu.name << endl;
+        cout << "Favourite sort of sex: " << isulu.preferences << endl;
+        cout << "One hour costs: " << isulu.price << " roubles" << endl;
+        cout << "The size of tits: " << isulu.boobiez_size << endl;
+        cout << "Her pussy can receive a cock with length equal to " << isulu.vaginas_depth << " cm" << endl;
     };
-    else if (pseudonym = "Nastya"){
-        cout << "Whore's name:" << nastya.name << endl;
-        cout << "Favourite sort of sex:" << nastya.preferences << endl;
-        cout << "One hour costs:" << nastya.price << "roubles" << endl;
-        cout << "The size of tits:" << nastya.boobiez_size << endl;
-        cout << "Her pussy can receive a cock with length equal to" << nastya.vaginas_depth << "cm" << endl;
+    else if (pseudonym == "Nastya"){
+        cout << "Whore's name: " << nastya.name << endl;
+        cout << "Favourite sort of sex: " << nastya.preferences << endl;
+        cout << "One hour costs: " << nastya.price << " roubles" << endl;
+        cout << "The size of tits: " << nastya.boobiez_size << endl;
+        cout << "Her pussy can receive a cock with length equal to " << nastya.vaginas_depth << " cm" << endl;
     };   
-    else if (pseudonym = "Shramko's mum"){
-        cout << "Whore's name:" << shramkosmum.name << endl;
-        cout << "Favourite sort of sex:" << shramkosmum.preferences << endl;
-        cout << "One hour costs:" << shramkosmum.price << "roubles" << endl;
-        cout << "The size of tits:" << shramkosmum.boobiez_size << endl;
-        cout << "Her pussy can receive a cock of" << shramkosmum.vaginas_depth << "cm" << endl;
+    else if (pseudonym == "Shramko's mum"){
+        cout << "Whore's name: " << shramkosmum.name << endl;
+        cout << "Favourite sort of sex :" << shramkosmum.preferences << endl;
+        cout << "One hour costs: " << shramkosmum.price << " roubles" << endl;
+        cout << "The size of tits: " << shramkosmum.boobiez_size << endl;
+        cout << "Her pussy can receive a cock of " << shramkosmum.vaginas_depth << " cm" << endl;
         };
     else {
         cout << "you have made a wrong choice" << endl;
